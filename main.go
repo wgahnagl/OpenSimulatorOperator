bf0e6c2f38c3977c73d74294eb9f94e147888036
Skyler Clark <skclark@redhat.com>
2023-11-17 13:05:58 -0500

    removes routes crd, I understand things now lol

--- main.go
+++ main.go
@@ -95,13 +95,6 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "OpenSimulator")
 		os.Exit(1)
 	}
-	if err = (&controllers.OpenSimulatorRouteReconciler{
-		Client: mgr.GetClient(),
-		Scheme: mgr.GetScheme(),
-	}).SetupWithManager(mgr); err != nil {
-		setupLog.Error(err, "unable to create controller", "controller", "OpenSimulatorRoute")
-		os.Exit(1)
-	}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
